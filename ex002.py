1d1e0676622107cdded2813f5b9f9fabfb455514
matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2021-03-09 06:54:26 -0300

    add file ex

---
+++ ex002.py
@@ -0,0 +1,4 @@
+nome=input('qual seu nome??')
+mes=input('qual mes vc nasceu?')
+ano=input('qual o ano que vc nasceu??')
+print('vc nasceu em', mes,'no ano',ano,'E seu nome é ',nome ,'correto?')
---
+++ ex003.py
@@ -0,0 +1,4 @@
+p1=input('primeiro numero')
+p2=input('segundo numero')
+result=int(p1)+int(p2)
+print('a soma é ',result)

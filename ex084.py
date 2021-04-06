347d807903a581b7aa84e3156f3c454b1437b807
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-04-06 16:06:31 -0300

    add file ex

---
+++ ex084.py
@@ -0,0 +1,30 @@
+temp = []
+princ = []
+mai = men = 0
+while True :
+    temp.append(str(input('nome: ')))
+    temp.append(float(input('peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Quer continuar ? [S/N] '))
+    if resp in 'nN':
+        break
+print('-='*30)
+print(f'ao todo foram {len(princ)} pessoas cadastradas')
+print(f'o maior peso foi de {mai} e foi de ',end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}]', end='')
+print()
+print(f'o menor peso foi de {men} e foi de ',end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}]', end='')
+print()

93d9cf2863a4e9667b011b1d7bc12bbcba6cda2d
Matheus Paixão Crisostenes <68738693+Matheuspaixaocrisostenes@users.noreply.github.com>
2021-04-12 10:23:42 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -2,6 +2,6 @@ n = int(input('Digite um numero : '))
 D = n * 2
 T = n *3
 R = n **(1/2)
-print('o dobra de {} vale {}'.format(n,D))
+print('o dobro de {} vale {}'.format(n,D))
 print('o triplo de {} vale {}'.format(n,T))
 print('a raiz quadrada de {} vale {:.2f} '.format(n,R))

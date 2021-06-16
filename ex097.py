52545f0980515d6278c9bb6abce25997e47e363d
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-06-16 15:31:42 -0300

    add new file ex

---
+++ ex097.py
@@ -0,0 +1,12 @@
+tam = 0
+def escreva(msg):
+    tam * len(msg) + 4
+    print('~'*tam)
+    print(f'  {msg}')
+    print('~'*tam)
+
+
+
+escreva('gustavo guanabara')
+escreva('cev')
+escreva('curso de python')

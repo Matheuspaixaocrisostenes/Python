949a3b9bdc84c17baf005a5e658c81af36ad94cb
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-06-24 07:39:06 -0300

    add new file ex

---
+++ ex100.py
@@ -0,0 +1,25 @@
+from time import sleep
+from random import randint
+
+
+def sorteia(lista):
+    print('sorteando 5 valores da lista.',end='')
+    for cont in range(0,5):
+        n = randint(1,10)
+        lista.append(n)
+        print(f'{n}',end=' ')
+        sleep(0.5)
+    print('acabou')
+
+
+def somap(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'somando os valores pares da lista {lista} , temos {soma}')
+
+
+numeros  = list()
+sorteia(numeros)
+somap(numeros)

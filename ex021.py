a175ef7e9b7562f1a52e436cadc5b73360f190f2
Matheus Paixão Crisostenes <matheuspcrisostenes@gmail.com>
2021-07-22 10:50:05 -0300

    modifed any files

--- ex021.py
+++ ex021.py
@@ -1,13 +1,12 @@
 '''import pygame
 pygame.init()
-pygame.mixer.music.load('icecrytal.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()'''
 import emoji
 from pygame import mixer
 mixer.init()
-mixer.music.load('niver.mp3')
+pygame.mixer.music.load('nome da musica vai aqui baixe um arquivo .mp3 e coloque aqui')
 mixer.music.play()
-print(emoji.emojize("eu te amoooooooo mãe :heart: ",use_aliases=True))
-input('parabéns para vc mãe obrigado por tudo que vc fez e faz por mim eu te amo muito por isso eu fiz esse programa para vc que seu dia seja noite no caso seja otima e amanhã tbm bom dia bu dii')
+print(emoji.emojize("Hello world :heart: ",use_aliases=True))
+input('Teste com musica')
 print(emoji.emojize("5 :heart: ",use_aliases=True))
